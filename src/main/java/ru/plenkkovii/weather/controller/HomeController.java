b56d1f6ad17668c3e7bb9011f3ca63a423c402ab
Ilya <ilplenkov@mail.ru>
2025-03-28 16:37:47 +0300

    шаблон стартовой страницы с thymeleaf

---
+++ src/main/java/ru/plenkkovii/weather/controller/HomeController.java
@@ -0,0 +1,14 @@
+package ru.plenkkovii.weather.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("name", "Илья");
+        return "home";
+    }
+}
